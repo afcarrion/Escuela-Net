78c714b9c4cad3eabefc84d458e22cc7a52f88da
afcarrion <af.carrion@uniandes.edu.co>
2020-02-09 16:58:23 -0500

    Adicion de  funcion lambda para eliminar cursos

--- Program.cs
+++ Program.cs
@@ -9,20 +9,23 @@ namespace EtapaUno
     {
         static void Main(string[] args)
         {
-            var miEscuela = new Entidades.Escuela("Agusti",1966);
-            miEscuela.Pais= "Colombia";
-            miEscuela.ciudad= "Bogota";
+            var miEscuela = new Entidades.Escuela("Agusti", 1966);
+            miEscuela.Pais = "Colombia";
+            miEscuela.ciudad = "Bogota";
             miEscuela.TipoEscuela = Entidades.TiposEscuela.Primaria;
 
             var arregloCursos = new Entidades.Curso[3];
-            var segundaEscuela = new Entidades.Escuela("Calazans",1980,EtapaUno.Entidades.TiposEscuela.Secundaria,ciudad:"Bogota");
-            var curso1 = new Entidades.Curso(){
-                Nombre="101",
+            var segundaEscuela = new Entidades.Escuela("Calazans", 1980, EtapaUno.Entidades.TiposEscuela.Secundaria, ciudad: "Bogota");
+            var curso1 = new Entidades.Curso()
+            {
+                Nombre = "101",
             };
-            var curso2 = new Entidades.Curso(){
+            var curso2 = new Entidades.Curso()
+            {
                 Nombre = "201",
             };
-            var curso3 = new Entidades.Curso(){
+            var curso3 = new Entidades.Curso()
+            {
                 Nombre = "301",
             };
 
@@ -63,21 +66,27 @@ namespace EtapaUno
             //otraListaCursos.Clear();
             miEscuela.CursosList = listaCursos;
 
-            miEscuela.CursosList.Add(new Curso{Nombre="103", Jornada= TiposJornada.Tarde });
-            miEscuela.CursosList.Add(new Curso{Nombre="203", Jornada= TiposJornada.Tarde });
+            miEscuela.CursosList.Add(new Curso { Nombre = "103", Jornada = TiposJornada.Tarde });
+            miEscuela.CursosList.Add(new Curso { Nombre = "203", Jornada = TiposJornada.Tarde });
 
             miEscuela.CursosList.AddRange(otraListaCursos);
 
-            Curso tmp = new Entidades.Curso(){Nombre = "101-vacacional", Jornada = TiposJornada.Noche};
+            Curso tmp = new Entidades.Curso() { Nombre = "101-vacacional", Jornada = TiposJornada.Noche };
             miEscuela.CursosList.Add(tmp);
             /*Remover algunos elementos*/
             imprimirSegundosCursos(miEscuela);
             miEscuela.CursosList.Remove(tmp);
             imprimirSegundosCursos(miEscuela);
 
-            /*Remover con predicado*/
-            Predicate<Curso> miAlgoritmo = Predicado;
-            miEscuela.CursosList.RemoveAll(miAlgoritmo);
+            /*Remover con delegado*/
+            miEscuela.CursosList.RemoveAll(delegate (Curso cur)
+            {
+                return cur.Nombre == "402";
+            });
+
+            /*Expresion lambda*/
+            miEscuela.CursosList.RemoveAll((cur) => cur.Nombre == "203" && cur.Jornada == TiposJornada.Tarde ); 
+            
             imprimirSegundosCursos(miEscuela);
         }
 
@@ -86,14 +95,16 @@ namespace EtapaUno
             return cur.Nombre == "402";
         }
 
-        private static void imprimirSegundosCursos(Escuela miEscuela){
+        private static void imprimirSegundosCursos(Escuela miEscuela)
+        {
             WriteLine("====== Lista de segundos cursos de la escuela ======");
             foreach (var curso in miEscuela.CursosList)
             {
                 Console.WriteLine($"Nombre: {curso.Nombre}");
             }
         }
-        private static void imprimirCursosEscuela(Curso[] cursos){
+        private static void imprimirCursosEscuela(Curso[] cursos)
+        {
             WriteLine("====== Lista de cursos de la escuela ======");
             foreach (var curso in cursos)
             {
@@ -106,20 +117,22 @@ namespace EtapaUno
             do
             {
                 Console.WriteLine($"Nombre: {arregloCursos[contador].Nombre} , ID: {arregloCursos[contador].UniqueID}");
-                contador ++;
+                contador++;
             } while (contador < arregloCursos.Length);
         }
 
         private static void imprimirCursosWhile(Curso[] arregloCursos)
         {
             int contador = 0;
-            while(contador < arregloCursos.Length){
+            while (contador < arregloCursos.Length)
+            {
                 Console.WriteLine($"Nombre {arregloCursos[contador].Nombre}, ID {arregloCursos[contador].UniqueID}");
                 contador = contador + 1;
             }
         }
 
-        private static void imprimirCursosForEach(Curso[] arregloCursos){
+        private static void imprimirCursosForEach(Curso[] arregloCursos)
+        {
             foreach (var curso in arregloCursos)
             {
                 Console.WriteLine($"Nombre: {curso.Nombre} , ID: {curso.UniqueID}");
